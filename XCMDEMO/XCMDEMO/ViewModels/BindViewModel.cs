27f3f2dfd9cc4cdc3dcba380e45f0cdc76d4d4a6
vladtf <vendeta0263@gmail.com>
2020-01-12 23:52:33 +0200

    Add a new view to test binding features Added a timer label to display an int counting implented using a task.

---
+++ XCMDEMO/XCMDEMO/ViewModels/BindViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XCMDEMO.ViewModels
+{
+    public class BindViewModel : Screen, IChildViewModel
+    {
+        public string DisplayName { get; set; } = "Bind Page";
+
+        public BindViewModel()
+        {
+
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Task.Delay(1000).Wait();
+                    Timer++;
+                }
+            });
+        }
+
+
+        private int _timer;
+
+        public int Timer
+        {
+            get { return _timer; }
+            set { Set(ref _timer, value); }
+        }
+
+
+
+    }
+}
