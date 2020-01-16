72d2bfae0505310226aafce1b94ff82f07c0cf1c
vladtf <vendeta0263@gmail.com>
2020-01-16 21:57:17 +0200

    Added a chat view to set up a new messenger sample

---
+++ XCMDEMO/XCMDEMO/ViewModels/ChatViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XCMDEMO.ViewModels
+{
+    public class ChatViewModel : Screen, IChildViewModel
+    {
+        public string Title { get; set; } = "Chat";
+        public ChatViewModel()
+        {
+
+        }
+
+
+    }
+}
