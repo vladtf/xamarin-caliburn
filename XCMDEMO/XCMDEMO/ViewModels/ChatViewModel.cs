e92b6797c321064f1fc685718819416b8f6142fe
vladtf <vendeta0263@gmail.com>
2020-01-17 00:02:00 +0200

    Added method on Completed and SendMessage to add CurrentMessage to the ListVIew ( + fixed list to bindablecollection issue )

--- XCMDEMO/XCMDEMO/ViewModels/ChatViewModel.cs
+++ XCMDEMO/XCMDEMO/ViewModels/ChatViewModel.cs
@@ -2,29 +2,44 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace XCMDEMO.ViewModels
 {
     public class ChatViewModel : Screen, IChildViewModel
     {
         public string Title { get; set; } = "Chat";
+        private BindableCollection<string> _messages;
+        private string _curMessage;
         public ChatViewModel()
         {
-            Messages = new List<string>
+            Messages = new BindableCollection<string>
             {
                 "Hello World", "New message", "Type something"
             };
+
+            CurrentMessage = "";
         }
 
 
-        private List<string> _messages;
 
-        public List<string> Messages
+        public string CurrentMessage
+        {
+            get { return _curMessage; }
+            set { Set(ref _curMessage, value); }
+        }
+
+        public BindableCollection<string> Messages
         {
             get { return _messages; }
             set { Set(ref _messages, value); }
         }
 
+        public void SendMessage()
+        {
+            Messages.Add(CurrentMessage);
+            CurrentMessage = "";
+        }
 
     }
 }
